20b5ab9900899a3136855346e4734945c5d2e4ab
Eloy <rojano.vargas.com>
2023-10-17 21:57:45 +0200

    finished useParams

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,22 @@
-import React from 'react'
-import { createRoot } from 'react-dom'
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import { createRoot } from "react-dom/client";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
-  //components will always be capitalized
-  return React.createElement(
-    "div", //what type of element we put in here, it could be, for example, a "p"
-    {}, //you can also put null instead of empty object
-    [
-      React.createElement("h1", {}, "Adopt Me!"), //this is the children of the previous div
-      React.createElement(Pet, {
-        animal: "Dog",
-        name: "Fosca",
-        breed: "Dalmatian-Labrator",
-      }),
-      React.createElement(Pet, {
-        animal: "Cat",
-        name: "Cocó",
-        breed: "Mixed",
-      }),
-      React.createElement(Pet, {
-        animal: "Rabbit",
-        name: "Bob Marley",
-        breed: "Mixed",
-      }),
-    ]
+  return (
+    <BrowserRouter>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+      </Routes>
+    </BrowserRouter>
   );
-}; // with this we have created a stamp but we have not used it. We have actually created a class but haven't created an instance yet
+};
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
--- zExamStudents.js
+++ zExamStudents.js
@@ -1,17 +1,93 @@
-// Task 1
+//TASK 1
+
+/*const profileData = {
+    firstName:'Skye',
+    lastName: 'Coore', 
+    age: 29, 
+    city: 'Japan'
+}*/
+
+//TASK 2
 
 const profileData = {
-    firstName: "Jess",
-    lastName: "Smith",
-    age: 30,
-    city: "Berlin",
-  };
-  
-  // Task 2
-  
-  profileData.details = function () {
-    return `${this.firstName} ${this.lastName} is ${this.age} years old and lives in ${this.city}`;
-  };
-  
-  console.log(profileData.details());
+    firstName:'Skye',
+    lastName: 'Coore', 
+    age: 29, 
+    city: 'Japan',
+    details: () => {
+        return `${profileData.firstName} ${profileData.lastName} is ${profileData.age} years old and lives in ${profileData.city}.`;
+    }
+}
+console.log (profileData.details())
 
+//TASK 3
+
+function wordConverter (words, suffix) {
+    const addSuffix = words.map (word => word + suffix);
+    return addSuffix;
+}
+
+let words = ["smart", "kind", "sweet", "small", "clear"];
+console.log(wordConverter(words, "er."))
+
+//TASK 4
+
+function calculateHours (hours) {
+  return hours.reduce((hours, day) => hours + day.end - day.start, 0)  
+}
+const hourTracking = [
+    { day: 'Monday', start: 8, end: 17},
+    { day: 'Tuesday', start: 9, end: 15},
+    { day: 'Wednesday', start: 10, end: 18},
+    { day: 'Thursday', start: 7, end: 14},
+    { day: 'Friday', start: 6, end: 12},
+];
+console.log(calculateHours(hourTracking))
+
+
+//TASK 5
+
+class Course {
+    constructor(teacher, type, number){
+        this.teacher = teacher;
+        this.type = type;
+        this.number = number;
+    }
+
+    spaceNeeded(){
+        return this.number * 2;
+    }
+
+    details(){
+        return `This is the ${this.type} course, taught by ${this.teacher}. There are currrently ${this.number} students on this course. `
+    }
+}
+
+const webDev = new Course('Mr Magoo', "Web Development", 18);
+
+console.log(webDev.spaceNeeded())
+console.log(webDev.details())
+
+//TASK 6
+
+function validPin(pin) {
+    if (!/^\d{4}$/.test(pin)) {
+      return false;
+    }
+  
+    const digits = pin.split('').map(Number); 
+  
+    const firstDigit = digits[0];
+    const lastDigit = digits[3];
+  
+    return !(firstDigit === lastDigit) && (lastDigit % 2 === 0);
+  }
+  
+  console.log(validPin("1234"));   
+  console.log(validPin("1235"));   
+  console.log(validPin("12ww"));   
+  console.log(validPin("12.4"));   
+  console.log(validPin("-234"));   
+  console.log(validPin("12345"));  
+  console.log(validPin("2222"));   
+  console.log(validPin("2224"));
